8a68c6765898a15034367e1d2103c7d04b502c18
swirlflag <swirlflag@gmail.com>
2022-07-09 21:28:17 +0900

    세팅 - scss 적용, global preload 작업

--- project/next.config.js
+++ project/next.config.js
@@ -4,32 +4,54 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 });
 
-const nextConfig = withBundleAnalyzer({
-    reactStrictMode: true,
+const path = require("path");
+
+const nextConfig = {
+	reactStrictMode: true,
 	swcMinify: true,
-    webpack(config, { webpack }) {
-        const isProd = process.env.NODE_ENV === "production";
-        const plugins = [...config.plugins];
-        if(isProd) {
-            // plugins.push()
-        }
-        // console.log({isProd});
-        return {
-            ...config,
-            mode: isProd ? "production" : "development",
-            devtool: isProd ? "hidden-source-map" : "eval",
-            plugins,
-            // module: {
-            //     ...config.module,
-            //     rules: [
-            //         ...config.module.rules,
-            //         {
 
-            //         }
-            //     ]
-            // }
-        }
-    }
-});
+    sassOptions: {
+        // includePaths: [path.join(__dirname, './')],
+        // prependData: `@import "./src/styles/variables.scss";`,
+        includePaths: [path.join(__dirname, './')],
+        prependData: `@import "./styles/variables.scss";`
+    },
 
-module.exports = nextConfig;
+	webpack(config, { webpack }) {
+        config.resolve.modules.push(__dirname);
+
+		const isProd = process.env.NODE_ENV === "production";
+		const plugins = [...config.plugins];
+
+        const alias = {
+            '@': path.resolve(__dirname, 'src'),
+            // '@services': path.resolve(__dirname, 'src/services'),
+            // '@utils': path.resolve(__dirname, 'src/utils'),
+            // '@hooks': path.resolve(__dirname, 'src/hooks'),
+            // '@components': path.resolve(__dirname, 'src/components')
+        }
+
+		if (isProd) {
+			// plugins.push()
+		}
+		// console.log({isProd});
+		return {
+			...config,
+			mode: isProd ? "production" : "development",
+			devtool: isProd ? "hidden-source-map" : "eval",
+			plugins,
+
+			// module: {
+			//     ...config.module,
+			//     rules: [
+			//         ...config.module.rules,
+			//         {
+
+			//         }
+			//     ]
+			// }
+		};
+	},
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
--- project/pages/_app.tsx
+++ project/pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-class Hey {
+// import "@/styles/variables.scss";
 
-}
+import "styles/test.scss";
+
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const a = 1;
 
-    
 	return <Component {...pageProps} />;
 }
 
